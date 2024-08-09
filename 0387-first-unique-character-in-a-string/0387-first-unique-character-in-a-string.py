79e807347c8a511dd608ae697851836af6c910fe
AdikeSidhartharao <116632230+AdikeSidhartharao@users.noreply.github.com>
2024-08-09 22:51:02 +0530

    Time: 133 ms (17.62%), Space: 17.1 MB (37.30%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        char_counts = OrderedDict()
+        for char in s:
+            char_counts[char] = char_counts.get(char,0)+1
+        for i,  char in enumerate(s):
+            if char_counts[char]==1:
+                return i
+        return -1
+        
